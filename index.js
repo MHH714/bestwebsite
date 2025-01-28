8eeb1dcd06cc4bc52449399e000f086f91762cb0
MHH714 <hirschmilesh@gmail.com>
2025-01-28 11:43:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
 console.log("JavaScript file loaded");
+
+const { Client, Intents, MessageEmbed } = require("discord.js");
+const client = new Client({
+    intents: [Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS],
+    ws: { properties: { $browser: "Discord iOS" } }
+})
+require("dotenv").config()
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+    client.user.setActivity(`Antiscam-Bot`, { type: 3, browser: "DISCORD IOS" })
+});
+client.on('messageCreate', async message => {
+    const array = require(`./scam.json`)
+    if (array.some(word => message.content.toLowerCase().includes(word))) {
+        message.delete()
+        const embed = new MessageEmbed()
+            .setTitle("Scam detected")
+            .setColor("#ff0000")
+            .setDescription("Please don't send any scam messages. Thank you.")
+            .setFooter("Antiscam-Bot by @HELLSNAKE#6851|Automatically delete messages after 1 minute")
+        message.channel.send({ embeds: [embed] }).then(m => { setTimeout(() => { m.delete() }, 60000) })
+    }
+})
+// anti crash 
+process.on("unhandledRejection", (reason, p) => {
+    console.log(reason, p)
+})
+process.on("uncaughtException", (err, origin) => {
+    console.log(err, origin)
+})
+client.login(process.env.token)
+
 #include <iostream>
 #include <stdlib.h>
 
